6503b90b4876b989780c2fbfb960366881bf0874
Zeroman <zeroman@losttd.com>
2015-11-17 01:42:59 +0800

    update make.sh

--- make.sh
+++ make.sh
@@ -7,6 +7,7 @@ download_files()
 {
     wget -c https://cygwin.com/setup-x86.exe
     wget -c http://iij.dl.osdn.jp/vlgothic/62375/VLGothic-20141206.tar.xz
+    tar xvf VLGothic-20141206.tar.xz VLGothic/VL-Gothic-Regular.ttf
 }
 
 
