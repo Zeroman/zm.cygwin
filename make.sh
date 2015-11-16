d4c5f20d7d31d67761ab8042752970fe926a35b2
Zeroman <zeroman@losttd.com>
2015-11-17 01:42:16 +0800

    update make.sh

--- make.sh
+++ make.sh
@@ -3,12 +3,13 @@
 # Zeroman Yang (51feel@gmail.com)
 # 11/13/2015
 
-download_cygwin()
+download_files()
 {
     wget -c https://cygwin.com/setup-x86.exe
+    wget -c http://iij.dl.osdn.jp/vlgothic/62375/VLGothic-20141206.tar.xz
 }
 
 
-download_cygwin
+download_files
 
 unix2dos *.bat 
